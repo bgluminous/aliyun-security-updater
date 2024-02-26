e91544967df1ed072e64c6413ef205d74bba9f29
BGLuminous <i@on.ink>
2024-02-26 11:36:35 +0800

    新特性(重试获取IP): 添加重试获取IP的功能和一些配置项
    - 新增 获取IP重试次数和间隔时间配置
    - 新增 获取IP返回Json节点名配置
    - 新增 一些配置项的默认参数
    - 修复 修复获取IPAPI返回Json错误时的错误提示
    - 优化 一些日志的等级

--- src/main/java/com/ik/central/aliyunsecurityupdater/utils/IpApiUtil.java
+++ src/main/java/com/ik/central/aliyunsecurityupdater/utils/IpApiUtil.java
@@ -1,10 +1,7 @@
 package com.ik.central.aliyunsecurityupdater.utils;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.heavenark.infrastructure.log.LogFactory;
-import com.heavenark.infrastructure.log.Logger;
 import com.ik.central.aliyunsecurityupdater.ConfigInstance;
 import com.ik.central.aliyunsecurityupdater.entity.CustomConfigPojo;
 
@@ -19,8 +16,6 @@ import java.util.Map;
  * 获取IP工具类
  */
 public class IpApiUtil {
-  private static final Logger LOGGER = LogFactory.getLogger(IpApiUtil.class);
-
   private static final CustomConfigPojo config = ConfigInstance.getConfig();
 
   private IpApiUtil() {
@@ -31,29 +26,16 @@ public class IpApiUtil {
    *
    * @return IP
    */
-  public static String getIp() {
-    String api = config.getIpApi() == null || config.getIpApi().isEmpty()
-      ? "https://api.kiiiv.com/v1/openapi/other/ip"
-      : config.getIpApi();
-    String resStr = "null";
-    try {
-      HttpClient client = HttpClient.newHttpClient();
-      HttpRequest request =
-        HttpRequest.newBuilder(URI.create(api)).build();
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      resStr = response.body();
-      ObjectMapper objectMapper = new ObjectMapper();
-      Map<String, String> node = objectMapper.readValue(resStr, new TypeReference<>() {
-      });
-      resStr = node.get("data");
-    } catch (JsonParseException ex) {
-      LOGGER.err(ex, "解析Json错误（；´д｀）ゞ错误信息:%s 原始数据：%s", ex.getMessage(), resStr);
-    } catch (IOException | InterruptedException ex) {
-      LOGGER.err(ex, "从OpenAPI获取IP错误（；´д｀）ゞ错误信息:%s", ex.getMessage());
-      Thread.currentThread().interrupt();
-    }
-    return resStr;
+  public static String getIp() throws IOException, InterruptedException {
+    HttpClient client = HttpClient.newHttpClient();
+    URI uri = URI.create(config.getIpApi());
+    HttpRequest request = HttpRequest.newBuilder(uri).build();
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    String res = response.body();
+    ObjectMapper objectMapper = new ObjectMapper();
+    Map<String, String> node = objectMapper.readValue(res, new TypeReference<>() {
+    });
+    return node.get(config.getResponseJsonIpNodeName());
   }
-
 
 }
